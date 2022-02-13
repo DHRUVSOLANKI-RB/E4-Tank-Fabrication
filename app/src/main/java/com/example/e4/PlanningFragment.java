7c1e255b10532340cc15acde9407ac493a36c8ea
DHRUVSOLANKI-RB <dhruv.solanki@railbit.in>
2022-02-13 20:05:25 +0530

    Tank Fabrication Complete GUI 12-02-22

--- app/src/main/java/com/example/e4/PlanningFragment.java
+++ app/src/main/java/com/example/e4/PlanningFragment.java
@@ -4,15 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 
 public class PlanningFragment extends Fragment {
 
+    TextView team_date;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planning, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_planning, container, false);
+
+        team_date = root.findViewById(R.id.team_date);
+        Button select_date = root.findViewById(R.id.select_date);
+        Button next_proc = root.findViewById(R.id.next_proc);
+
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder.setTitleText("SELECT DATE");
+
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+
+        select_date.setOnClickListener(v -> {
+            materialDatePicker.show(getActivity().getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(
+                selection -> team_date.setText(materialDatePicker.getHeaderText()));
+
+        next_proc.setOnClickListener(view -> {
+
+            //Toast.makeText(getActivity(), "Please fill all form fields.", Toast.LENGTH_LONG).show();
+            Navigation.findNavController(view).navigate(R.id.nav_tankfab);
+        });
+
+        return root;
     }
 }
---
+++ app/src/main/java/com/example/e4/TankFabFragment.java
@@ -0,0 +1,64 @@
+package com.example.e4;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+public class TankFabFragment extends Fragment {
+
+    TextView mounted_date_text;
+    TextView assigned_date_text;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_tank_fab, container, false);
+
+        mounted_date_text = root.findViewById(R.id.mounted_date_text);
+        Button mounted_date = root.findViewById(R.id.mounted_date);
+        assigned_date_text = root.findViewById(R.id.assigned_date_text);
+        Button assigned_date = root.findViewById(R.id.assigned_date);
+        Button next_tankfeb = root.findViewById(R.id.next_tankfeb);
+
+        MaterialDatePicker.Builder materialDateBuilder_mounted = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder_mounted.setTitleText("MOUNTED ON VEHICLE DATE");
+
+        final MaterialDatePicker materialDatePicker_mounted = materialDateBuilder_mounted.build();
+
+        mounted_date.setOnClickListener(v -> {
+            materialDatePicker_mounted.show(getActivity().getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+        });
+
+        materialDatePicker_mounted.addOnPositiveButtonClickListener(
+                selection -> mounted_date_text.setText(materialDatePicker_mounted.getHeaderText()));
+
+        /*     Assigned Date     */
+
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder.setTitleText("ASSIGNED DATE");
+
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+
+        assigned_date.setOnClickListener(v -> {
+            materialDatePicker.show(getActivity().getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(
+                selection -> assigned_date_text.setText(materialDatePicker.getHeaderText()));
+
+        next_tankfeb.setOnClickListener(view -> {
+
+            Navigation.findNavController(view).navigate(R.id.nav_tankfab);
+        });
+
+        return root;
+    }
+}
