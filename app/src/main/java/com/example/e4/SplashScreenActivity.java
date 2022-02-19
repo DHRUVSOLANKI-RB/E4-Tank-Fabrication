168ac92d9551f31bd3f77623a487f6e9cc72e503
DHRUVSOLANKI-RB <dhruv.solanki@railbit.in>
2022-02-19 19:08:25 +0530

    Splash screen completed on 19-02-2022

--- app/src/main/java/com/example/e4/SplashScreenActivity.java
+++ app/src/main/java/com/example/e4/SplashScreenActivity.java
@@ -1,17 +1,31 @@
 package com.example.e4;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
+
+    public static final String MyPREFERENCES = "MyPrefs";
+    SharedPreferences sp_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreenActivity.this, UserLoginActivity.class));
+
+        sp_login = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        if (sp_login.getBoolean("logged", false)) {
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        }else{
+            startActivity(new Intent(SplashScreenActivity.this, UserLoginActivity.class));
+        }
+
         finish();
     }
 }
