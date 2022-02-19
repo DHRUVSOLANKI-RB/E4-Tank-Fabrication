64202394df1f918096a414baea05337459f68233
DHRUVSOLANKI-RB <dhruv.solanki@railbit.in>
2022-02-19 19:04:50 +0530

    Splash screen completed on 19-02-2022

---
+++ app/src/main/java/com/example/e4/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.e4;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, UserLoginActivity.class));
+        finish();
+    }
+}
