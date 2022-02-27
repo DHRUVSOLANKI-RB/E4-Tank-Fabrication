5ca93519c680f251e3910958acaaac82c346022a
DHRUVSOLANKI-RB <dhruv.solanki@railbit.in>
2022-02-27 16:29:19 +0530

    Checklist - Pre Delivery completed on 27-02-22

--- app/src/main/java/com/example/e4/CheckistPreDeliveryFragment.java
+++ app/src/main/java/com/example/e4/CheckistPreDeliveryFragment.java
@@ -1,18 +1,42 @@
 package com.example.e4;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.example.e4.ui.upload.HomeFragment;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 
 public class CheckistPreDeliveryFragment extends Fragment {
 
-
+    String HttpURL = "http://3.222.104.176/index.php/checklistpredelivery";
+    HashMap<String, String> hashMap = new HashMap<>();
+    com.example.e4.HttpParse httpParse = new com.example.e4.HttpParse();
+    ProgressDialog progressDialog;
+    String finalResult;
+    
+    CheckBox tank_mounting,safety_fittings,valve_lever,locking,color,sticker,number_plates,accessories;
+    String txt_tank_mounting = "",txt_safety_fittings = "",txt_valve_lever = "",txt_locking = "",txt_color = "",txt_sticker = "",txt_number_plates = "",
+            txt_accessories = "",vehicle_unid = "";
+    
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -21,16 +45,117 @@ public class CheckistPreDeliveryFragment extends Fragment {
         Button prev_predelivery = root.findViewById(R.id.prev_predelivery);
         Button next_predelivery = root.findViewById(R.id.next_predelivery);
 
+        tank_mounting = root.findViewById(R.id.tank_mounting);
+        safety_fittings = root.findViewById(R.id.safety_fittings);
+        valve_lever = root.findViewById(R.id.valve_lever);
+        locking = root.findViewById(R.id.locking);
+        color = root.findViewById(R.id.color);
+        sticker = root.findViewById(R.id.sticker);
+        number_plates = root.findViewById(R.id.number_plates);
+        accessories = root.findViewById(R.id.accessories);
+        
         prev_predelivery.setOnClickListener(view -> {
 
             Navigation.findNavController(view).navigate(R.id.nav_touchup);
         });
 
+        SharedPreferences sharedpreferences = getActivity().getSharedPreferences(HomeFragment.MyPREFERENCES, Context.MODE_PRIVATE);
+        vehicle_unid = sharedpreferences.getString("unid","");
+
         next_predelivery.setOnClickListener(view -> {
 
-            Navigation.findNavController(view).navigate(R.id.nav_checklistpredelivery);
+            txt_tank_mounting = tank_mounting.getText().toString();
+            txt_safety_fittings = safety_fittings.getText().toString();
+            txt_valve_lever = valve_lever.getText().toString();
+            txt_locking = locking.getText().toString();
+            txt_color = color.getText().toString();
+            txt_accessories = accessories.getText().toString();
+            txt_number_plates = number_plates.getText().toString();
+            txt_sticker = sticker.getText().toString();
+
+            UserLoginFunction(view);
         });
 
         return root;
+    }
+
+    public void UserLoginFunction(View view) {
+
+        class UserLoginClass extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                progressDialog = ProgressDialog.show(getActivity(), "Loading Data", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+
+                super.onPostExecute(httpResponseMsg);
+
+                progressDialog.dismiss();
+
+                //Toast.makeText(com.example.e4.UserLoginActivity.this,httpResponseMsg, Toast.LENGTH_LONG).show();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(httpResponseMsg);
+
+                    new MaterialAlertDialogBuilder(getContext()).setTitle(jsonObject.getString("statusMessage")).setPositiveButton("Ok", (dialogInterface, i) -> {
+
+                        String error = null;
+                        try {
+                            error = jsonObject.getString("status");
+
+                            if (error.equals("success")) {
+
+                                progressDialog.dismiss();
+
+                                Navigation.findNavController(view).navigate(R.id.nav_checklistpredelivery);
+
+//                                    filename.setText("");
+//                                    txt_rdsospecs.getText().clear();
+//                                    txt_vendor.getText().clear();
+//                                    txt_vendorid.getText().clear();
+//                                    txt_fileno.getText().clear();
+//                                    txt_itemname.getText().clear();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                hashMap.put("tank_mounting", txt_tank_mounting);
+                hashMap.put("safety_fittings", txt_safety_fittings);
+                hashMap.put("valve_lever", txt_valve_lever);
+                hashMap.put("locking", txt_locking);
+                hashMap.put("color", txt_color);
+                hashMap.put("sticker", txt_sticker);
+                hashMap.put("number_plates", txt_number_plates);
+                hashMap.put("accessories", txt_accessories);
+                hashMap.put("vehicle_unid", vehicle_unid);
+
+                finalResult = httpParse.postRequest(hashMap, HttpURL);
+
+                Log.d("doInBackground: ", finalResult);
+                return finalResult;
+            }
+        }
+
+        UserLoginClass userLoginClass = new UserLoginClass();
+
+        userLoginClass.execute();
     }
 }
